2929ae34503c0f29ff7425e029ee010898bfd44c
Curt Spark <git@cspark.dev>
2024-11-25 17:39:40 +0000

    Initial Commit

---
+++ IOSTests/IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTests.swift
+//  IOSTests
+//
+//  Created by Curt Spark on 25/11/2024.
+//
+
+import Testing
+@testable import IOS
+
+struct IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
