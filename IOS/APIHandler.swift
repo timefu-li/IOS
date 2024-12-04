2f12a058c52ee5522e5f191eda784cf86fc852a3
Curt Spark <git@cspark.dev>
2024-12-04 15:40:00 +0000

    Break out request handler into its own function

--- IOS/APIHandler.swift
+++ IOS/APIHandler.swift
@@ -16,23 +16,29 @@ struct APIHandler {
         case decodeBackendErrorResponseError
     }
 
-    static public func fetchTasks() async throws(APIHandlerError) -> [Task] {
-        guard let url = URL(string: "http://localhost:8080/tasks") else {
+    static private func attemptRequest(url: String, method: String) async throws(APIHandlerError) -> Data {
+        guard let urlObject = URL(string: url) else {
             throw APIHandlerError.urlCheckFail
         }
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        var request = URLRequest(url: urlObject)
+        request.httpMethod = method
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
-        var URLRequest: (Data: Data, URLResponse: URLResponse)
+        var requestOutcome: (Data: Data, URLResponse: URLResponse)
         do {
-            URLRequest = try await URLSession.shared.data(from: url)
+            requestOutcome = try await URLSession.shared.data(from: urlObject)
         } catch {
             throw APIHandlerError.urlRequestFail(reasonPhrase: error.localizedDescription)
         }
+
+        return requestOutcome.Data
+    }
+
+    static public func fetchTasks() async throws(APIHandlerError) -> [Task] {
+        let jsondata = try await attemptRequest(url: "http://localhost:8080/tasks", method: "GET")
  
-        guard let result: [Task] = try? JSONDecoder().decode([Task].self, from: URLRequest.Data) else {
-            guard let resulterror: BackendError = try? JSONDecoder().decode(BackendError.self, from: URLRequest.Data) else {
+        guard let result: [Task] = try? JSONDecoder().decode([Task].self, from: jsondata) else {
+            guard let resulterror: BackendError = try? JSONDecoder().decode(BackendError.self, from: jsondata) else {
                 throw APIHandlerError.decodeBackendErrorResponseError
             }
 
