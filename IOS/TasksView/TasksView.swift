0b300127b9ec4ef580b355eaa441272e8217095d
Curt Spark <git@cspark.dev>
2024-12-05 11:20:55 +0000

    Init tasks view

---
+++ IOS/TasksView/TasksView.swift
@@ -0,0 +1,66 @@
+//
+//  TasksView.swift
+//  IOS
+//
+//  Created by Curt Spark on 05/12/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct TasksView: View {
+    @State var tasks: [Task]
+    @State var errorstate: any Error
+
+    init() {
+        tasks = []
+        errorstate = APIHandler.APIHandlerError.OK
+    }
+
+    var body: some View {
+        VStack {
+
+            // Tasks list
+            Group(content: {
+                    switch errorstate {
+                        case APIHandler.APIHandlerError.OK:
+                            List(tasks, id: \.self, rowContent: { (taskelement: Task) in
+                                Button("\(taskelement.category?.emoji ?? "")  \(taskelement.name ?? "No name found!")", action: {
+                                    print(taskelement.name ?? "No name found!")
+                                })
+                            })
+                        case APIHandler.APIHandlerError.decodeModelError(reason: "NOTFOUND:No tasks found"):
+                            Text("No tasks found!")
+                        default:
+                            Text("Received following error!")
+                            Text("\(errorstate.self) : \(errorstate.localizedDescription)")
+                    }
+            }).task({ () async -> Void in
+                do {
+                    tasks = try await APIHandler.getTasks()
+                    print("Fetched following tasks...")
+                    print(tasks)
+                } catch {
+                    tasks = []
+                    errorstate = error.self
+                    print("Encountered the following error fetching tasks!")
+                    print("\(error.self) : \(error.localizedDescription)")
+                }
+            })
+            //HStack {
+            //    Button("New Item", action: {
+            //        test.append("Wooo")
+            //    })
+            //        .frame(width: 50, height: 50)
+            //    Button("New Item 2", action: {
+            //        test.append("Wooo")
+            //    })
+            //        .frame(width: 50, height: 50)
+            //}
+        }
+    }
+}
+
+#Preview {
+    TasksView()
+}
