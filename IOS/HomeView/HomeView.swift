0011b7b09558c43c432e044967453d8e26c60ab5
Curt Spark <git@cspark.dev>
2024-12-05 14:37:25 +0000

    Init home recent view

--- IOS/HomeView/HomeView.swift
+++ IOS/HomeView/HomeView.swift
@@ -11,28 +11,34 @@ import SwiftData
 struct HomeView: View {
     @State var tasks: [Task]
     @State var errorstate: any Error
+    @State var taskelapsed: Date
 
     init() {
         tasks = []
         errorstate = APIHandler.APIHandlerError.OK
+        taskelapsed = Date.now
     }
 
     var body: some View {
         VStack {
 
-            Button(Date.now.formatted(date: .omitted, time: .shortened), action: {
-                print(Date.now.formatted(date: .omitted, time: .shortened))
+            Button(taskelapsed.formatted(date: .omitted, time: .shortened), action: {
+                print(taskelapsed.formatted(date: .omitted, time: .shortened))
             })
                 .font(.system(size: 72))
 
             TabView {
                 TasksView()
                     .tabItem({
-                        Label("Tasks", systemImage: "1.circle")
+                        Label("Tasks", systemImage: "2.circle")
+                    })
+                HomeRecentView()
+                    .tabItem({
+                        Label("Home", systemImage: "1.circle")
                     })
                 CategoriesView()
                     .tabItem({
-                        Label("Categories", systemImage: "2.circle")
+                        Label("Categories", systemImage: "3.circle")
                     })
             }
 
